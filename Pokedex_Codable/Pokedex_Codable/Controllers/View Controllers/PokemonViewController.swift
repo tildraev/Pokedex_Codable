70bd940cd506e9f5156a83968631c9340c07653d
Arian Mohajer <amohajer45@gmail.com>
2022-02-08 10:25:19 -0700

    Pokedex and pokedex result models made

--- Pokedex_Codable/Pokedex_Codable/Controllers/View Controllers/PokemonViewController.swift
+++ Pokedex_Codable/Pokedex_Codable/Controllers/View Controllers/PokemonViewController.swift
@@ -17,56 +17,56 @@ class PokemonViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pokemonMovesTableView.delegate = self
-        pokemonMovesTableView.dataSource = self
-        pokemonSearchBar.delegate = self
+//        pokemonMovesTableView.delegate = self
+//        pokemonMovesTableView.dataSource = self
+//        pokemonSearchBar.delegate = self
     }
     
-    var pokemon: Pokemon?
-    
-    func updateViews(for pokemon: Pokemon) {
-        NetworkingController.fetchImage(for: pokemon) { image in
-            guard let image = image else {return}
-            DispatchQueue.main.async {
-                self.pokemon = pokemon
-                self.pokemonSpriteImageView.image = image
-                self.pokemonIDLabel.text = ("No:\(pokemon.id)")
-                self.pokemonNameLabel.text = pokemon.name.capitalized
-                self.pokemonMovesTableView.reloadData()
-            }
-        }
-    }
+//    var pokemon: Pokemon?
+//
+//    func updateViews(for pokemon: Pokemon) {
+//        NetworkingController.fetchImage(for: pokemon) { image in
+//            guard let image = image else {return}
+//            DispatchQueue.main.async {
+//                self.pokemon = pokemon
+//                self.pokemonSpriteImageView.image = image
+//                self.pokemonIDLabel.text = ("No:\(pokemon.id)")
+//                self.pokemonNameLabel.text = pokemon.name.capitalized
+//                self.pokemonMovesTableView.reloadData()
+//            }
+//        }
+//    }
     
 }// End
 
 
-extension PokemonViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Moves"
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pokemon?.moves.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "moveCell", for: indexPath)
-        guard let pokemon = pokemon else {return UITableViewCell() }
-        let move = pokemon.moves[indexPath.row]
-        cell.textLabel?.text = move
-        return cell
-    }
-}
-
-extension PokemonViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        NetworkingController.fetchPokemon(with: searchText) { pokemon in
-            guard let pokemon = pokemon else {
-                return
-            }
-            self.updateViews(for: pokemon)
-        }
-    }
-}
+//extension PokemonViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "Moves"
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return pokemon?.moves.count ?? 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "moveCell", for: indexPath)
+//        guard let pokemon = pokemon else {return UITableViewCell() }
+//        let move = pokemon.moves[indexPath.row]
+//        cell.textLabel?.text = move
+//        return cell
+//    }
+//}
+//
+//extension PokemonViewController: UISearchBarDelegate {
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        NetworkingController.fetchPokemon(with: searchText) { pokemon in
+//            guard let pokemon = pokemon else {
+//                return
+//            }
+//            self.updateViews(for: pokemon)
+//        }
+//    }
+//}
--- Pokedex_Codable/Pokedex_Codable/Model/Pokemon.swift
+++ Pokedex_Codable/Pokedex_Codable/Model/Pokemon.swift
@@ -7,41 +7,17 @@
 
 import Foundation
 
-class Pokemon {
-    enum Keys: String {
-        case name = "name"
-        case id = "id"
-        case moves = "moves"
-        case sprites = "sprites"
-        case frontShiny = "front_shiny"
-        case move = "move"
-    }
+struct Pokedex: Decodable {
+    
+    let results: [PokedexResult]
+}
+
+struct PokedexResult: Decodable {
     
     let name: String
-    let id: Int
-    let moves: [String]
-    let spritePath: String
-    
-    init?(dictionary:[String:Any]) {
-        guard let name = dictionary[Keys.name.rawValue] as? String,
-              let id = dictionary[Keys.id.rawValue] as? Int,
-              let spriteDict = dictionary[Keys.sprites.rawValue] as? [String:Any],
-              let spritePosterPath = spriteDict[Keys.frontShiny.rawValue] as? String,
-              let movesArray = dictionary[Keys.moves.rawValue] as? [[String:Any]] else {return nil}
-        
-        var moves: [String] = []
-        
-        for dict in movesArray {
-            guard let moveDict = dict[Keys.move.rawValue] as? [String:Any],
-                  let moveName = moveDict[Keys.name.rawValue] as? String else {return nil}
-            
-            moves.append(moveName)
-        }
-        
-        self.name = name
-        self.id = id
-        self.moves = moves
-        self.spritePath = spritePosterPath
-        
-    }
+    let urlString: String
 }
+
+
+
+
