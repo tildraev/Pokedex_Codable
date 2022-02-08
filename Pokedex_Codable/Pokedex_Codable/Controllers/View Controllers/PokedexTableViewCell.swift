c083e9f26217e24582231cdb034e31eeedfafac7
Arian Mohajer <amohajer45@gmail.com>
2022-02-08 10:17:11 -0700

    new tableview UI laid out with corresponding controllers (view and cell)

---
+++ Pokedex_Codable/Pokedex_Codable/Controllers/View Controllers/PokedexTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokedexTableViewCell.swift
+//  Pokedex_Codable
+//
+//  Created by Arian Mohajer on 2/8/22.
+//
+
+import UIKit
+
+class PokedexTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
